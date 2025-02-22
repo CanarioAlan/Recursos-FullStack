aa0c772531fb71bd92113b23d5cff363dcf000d5
Canario Alan <alan.union.1998@gmail.com>
2025-02-22 11:01:00 -0300

    Agregando la validacion del formulario

--- Frontend/Formulario/app.js
+++ Frontend/Formulario/app.js
@@ -15,11 +15,51 @@ document.addEventListener("DOMContentLoaded", function () {
   const inputMensaje = document.querySelector("#mensaje");
   const btnSubmit = document.querySelector("#enviar");
   //asignar evento
-  inputNombre.addEventListener("input", validar);
+  inputNombre.addEventListener("blur", validar);
   inputApellido.addEventListener("input", validar);
   inputemail.addEventListener("input", validar);
   inputTelefono.addEventListener("input", validar);
   inputMensaje.addEventListener("input", validar);
   btnSubmit.addEventListener("click", enviarEmail);
-  //validar campos
+  //comprbar campos
+  function enviarEmail(e) {}
+  function validar(e) {
+    // trim se utiliza para eliminar espacios en blanco
+    if (e.target.value.trim() == "") {
+      crearAlerta(
+        `El campo ${e.target.id} es obligatorio`,
+        e.target.parentElement
+      );
+      email[e.target.name] = "";
+      habilitarBtn();
+    }
+  }
+  //Crear alerta -- Creamos un parrafo y le agregamos los estiloss
+  function crearAlerta(mensaje, referencia) {
+    const alerta = document.createElement("P");
+    alerta.textContent = mensaje;
+    alerta.classList.add("alerta");
+    eliminarAlerta(referencia);
+
+    referencia.appendChild(alerta);
+  }
+  //Verificamos si existe la clase alerta, si esxite la eliminamos y si no la creamos, para que no se dupliquen
+  function eliminarAlerta(referencia) {
+    const existe = referencia.querySelector(".alerta");
+    if (existe) {
+      existe.remove();
+    }
+  }
+  //ferificar el Email valido
+
+  //avilitar boton
+  function habilitarBtn() {
+    if (Object.values(email).includes("")) {
+      btnSubmit.disabled = true;
+      btnSubmit.classList.add("opacity");
+      return;
+    }
+    btnSubmit.disabled = false;
+    btnSubmit.classList.remove("opacity");
+  }
 });
